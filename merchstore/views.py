04132898440fe3110e51ed44e0e3a8f1b2ce5029
jamesmostajo <jipmostajo@gmail.com>
2024-03-14 14:47:18 +0800

    views for merchstore added

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Product, ProductType
+
+def products_list(request):
+    products = Product.objects.all()
+    ctx = {'products_list' : products}
+    return render(request, 'product_list.html', ctx)
+
+def product(request, pk):
+    product = Product.objects.get(pk = pk)
+    ctx = {
+        'name': product.name,
+        'description' : product.description,
+        'price' : product.price,
+        'product_type' : product.product_type.name
+    }
+    return render(request, 'product_item.html', ctx)
