4ef4c05639058b324b1880616e6a0459ab2a7f45
jamesmostajo <jipmostajo@gmail.com>
2024-03-15 18:29:17 +0800

    added helper functions to models and modified admin

--- merchstore/models.py
+++ merchstore/models.py
@@ -6,6 +6,9 @@ class ProductType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering = ["name"]
 
@@ -17,6 +20,9 @@ class Product(models.Model):
     )
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
 
     def get_absolute_url(self):
         return reverse("merchstore:product_detail", args=[str(self.pk)])
