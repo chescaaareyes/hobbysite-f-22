fdf03cf8780cc8b11bbae5305afdb3c06c15c563
jamesmostajo <jipmostajo@gmail.com>
2024-03-15 18:13:44 +0800

    adjusted urls, settings, and views to match templates

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,25 +1,25 @@
 from django.db import models
+from django.urls import reverse
 
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
-
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
 
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     product_type = models.ForeignKey(
-        'ProductType',
-        on_delete = models.SET_NULL,
-        related_name = 'product_type'
+        "ProductType", on_delete=models.SET_NULL, null=True, related_name="product_type"
     )
     description = models.TextField()
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def get_absolute_url(self):
+        return reverse("merchstore:product_detail", args=[str(self.pk)])
 
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
--- merchstore/views.py
+++ merchstore/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Product, ProductType
 
+
 def products_list(request):
     products = Product.objects.all()
-    ctx = {'products_list' : products}
-    return render(request, 'product_list.html', ctx)
+    ctx = {"products_list": products}
+    return render(request, "product_list.html", ctx)
+
 
 def product(request, pk):
-    product = Product.objects.get(pk = pk)
+    product = Product.objects.get(pk=pk)
     ctx = {
-        'name': product.name,
-        'description' : product.description,
-        'price' : product.price,
-        'product_type' : product.product_type.name
+        "name": product.name,
+        "description": product.description,
+        "price": product.price,
+        "product_type": product.product_type.name,
     }
-    return render(request, 'product_item.html', ctx)
+    return render(request, "product_detail.html", ctx)
