037397da589ad8ed6dacda2c662ba39aaa7c504d
Francesca Reyes <francesca.dominique.reyes@student.ateneo.edu>
2024-03-19 20:57:36 +0800

    Add admins for the Comment and Commission models

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Commission
+  
+class CommentAdmin(admin.ModelAdmin):
+  model = Comment
+  search_fields = ("entry", )
+
+class CommentInline(admin.TabularInline):
+  model = Comment
+  
+class CommissionAdmin(admin.ModelAdmin):
+  model = Commission
+  search_fields = ("title", )
+  inlines = [CommentInline]
+  
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Commission, CommissionAdmin)
