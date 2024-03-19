04de634d6e885e758f0d2ff3f889203e44dbd42b
Francesca Reyes <francesca.dominique.reyes@student.ateneo.edu>
2024-03-19 12:46:37 +0800

    Create views for commissions app

--- commissions/views.py
+++ commissions/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Comment, Commission
+
+
+def commissions_list(request):
+    commissions = Commission.objects.all().order_by("created_on")
+    ctx = {"commissions": commissions}
+    return render(request, "commissions/commissions_list.html", ctx)
+
+
+def commission(request, pk):
+    commission_title = Commission.objects.get(pk=pk)
+    comments = Comment.objects.filter(commission__pk=pk).order_by("-created_on")
+    ctx = {"commission": commission_title, "comments": comments}
+    return render(request, "commissions/commission.html", ctx)
