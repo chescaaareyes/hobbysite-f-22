58448e733930a65c24758e7def417e9ca84028f8
Francesca Reyes <francesca.dominique.reyes@student.ateneo.edu>
2024-03-19 21:12:14 +0800

    Fix formatting in admin.py

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,18 +1,22 @@
 from django.contrib import admin
 
 from .models import Comment, Commission
-  
+
+
 class CommentAdmin(admin.ModelAdmin):
-  model = Comment
-  search_fields = ("entry", )
+    model = Comment
+    search_fields = ("entry",)
+
 
 class CommentInline(admin.TabularInline):
-  model = Comment
-  
+    model = Comment
+
+
 class CommissionAdmin(admin.ModelAdmin):
-  model = Commission
-  search_fields = ("title", )
-  inlines = [CommentInline]
-  
+    model = Commission
+    search_fields = ("title",)
+    inlines = [CommentInline]
+
+
 admin.site.register(Comment, CommentAdmin)
 admin.site.register(Commission, CommissionAdmin)
