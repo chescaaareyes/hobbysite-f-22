de95e4f6f2a83642f6734aa79a78f8492a49bb7b
Francesca Reyes <francesca.dominique.reyes@student.ateneo.edu>
2024-03-19 21:32:01 +0800

    Remove ordering in view.py

--- commissions/views.py
+++ commissions/views.py
@@ -4,13 +4,13 @@ from .models import Comment, Commission
 
 
 def commissions_list(request):
-    commissions = Commission.objects.all().order_by("created_on")
+    commissions = Commission.objects.all()
     ctx = {"commissions": commissions}
     return render(request, "commissions/commissions_list.html", ctx)
 
 
 def commission(request, pk):
     commission_title = Commission.objects.get(pk=pk)
-    comments = Comment.objects.filter(commission__pk=pk).order_by("created_on")
+    comments = Comment.objects.filter(commission__pk=pk)
     ctx = {"commission": commission_title, "comments": comments}
     return render(request, "commissions/commission.html", ctx)
