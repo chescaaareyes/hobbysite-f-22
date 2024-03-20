441451f825d55f8a6a4eb42bc1e9cd9eea1c5f47
poropirar <paulo.pilar@student.ateneo.edu>
2024-03-20 12:34:42 +0900

    Edited in all the requirments (PRE-DEBUG_

---
+++ wiki/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.urls import reverse
+
+
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        "ArticleCategory", 
+        on_delete=models.SET_NULL, 
+        null=True,
+        related_name="article_category",
+        )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True, null = True)
+    updated_on = models.DateTimeField(auto_now=True, null = True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("wiki:article_detail", args=[int(self.pk)])
+
---
+++ wiki/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import articles_list_view,article_detail
+
+urlpatterns = [
+    path('wiki/articles', articles_list_view, name ='article_list' ),
+    path('wiki/article/<int:articles_detail>', article_detail, name='article_detail'),
+
+]
+
+# This might be needed, depending on your Django version
+app_name = "wiki"
