f45246ab1a947af4566f3c4e5542f785e9239a78
gigigar <133097398+gigigar@users.noreply.github.com>
2024-03-20 16:59:50 +0800

    Updated views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Article, ArticleCategory
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'blog/article_list.html'
+
+
+class ArticleDetailsView(DetailView):
+    model = Article
+    template_name = 'blog/article_details.html'
