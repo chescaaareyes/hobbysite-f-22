bce81a7ccabe10c183605ce732b25a987c58cd3c
poropirar <paulo.pilar@student.ateneo.edu>
2024-05-05 00:08:29 +0900

    Added Comments

--- wiki/models.py
+++ wiki/models.py
@@ -34,3 +34,25 @@ class Article(models.Model):
     def get_absolute_url(self):
         return reverse("wiki:article_detail", args=[int(self.pk)])
 
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null = True,
+        related_name="user_comments"
+    )
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        null = True,
+        related_name="article_comments",
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, null = True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.author.username} for {self.article.title}"
--- wiki/views.py
+++ wiki/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 
-from .models import Article
-from .forms import ArticleForm
+from .models import Article, Comment
+from .forms import ArticleForm, CommentForm
 
 def article_list(request):
     articles = Article.objects.order_by("-created_on")
@@ -13,12 +13,29 @@ def article_list(request):
 @login_required
 def article_detail(request, pk):
     article = Article.objects.get(pk=pk)
+    comments = Comment.objects.filter(article=article)
+    comment_form = CommentForm()
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.article = article
+            new_comment.author = request.user  
+            new_comment.save()
+            return redirect('wiki:article_detail', pk=pk)
+        
+        else:
+            comment_form = CommentForm()
+            
     context = {
         "title": article.title,
         "category": article.category.name,
         "created_on": article.created_on,
         "updated_on": article.updated_on,
         "entry": article.entry,
+        "article": article,
+        "comments": comments,
+        "comment_form": comment_form,
     }
     return render(request, "wiki/article_detail.html", context)
 
@@ -30,7 +47,7 @@ def article_create(request):
         if form.is_valid():
             article = Article()
             article.title = form.cleaned_data.get("title")
-            article.category.name = form.cleaned_data.get("category")
+            article.category = form.cleaned_data.get("category")
             article = form.save()
             return redirect("wiki:article_detail", pk=article.pk)
         
