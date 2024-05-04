1450dd56acaa10c3870f5f7ea792888055c7a5f6
poropirar <paulo.pilar@student.ateneo.edu>
2024-05-04 22:50:21 +0900

    added creating and updating articles

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import article_detail, article_list
+from .views import article_detail, article_list, article_create, article_update
 
 urlpatterns = [
     path("articles", article_list, name="article_list"),
     path("article/<int:pk>/", article_detail, name="article_detail"),
+    path('article/add/', article_create, name="article_create"),
+    path('article/<int:pk>/edit', article_update, name="article_update"),
+
 ]
 
 app_name = "wiki"
--- wiki/views.py
+++ wiki/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
 
 from .models import Article
-
+from .forms import ArticleForm
 
 def article_list(request):
     articles = Article.objects.order_by("-created_on")
     context = {"articles": articles}
     return render(request, "wiki/article_list.html", context)
 
-
+@login_required
 def article_detail(request, pk):
     article = Article.objects.get(pk=pk)
     context = {
@@ -19,3 +21,32 @@ def article_detail(request, pk):
         "entry": article.entry,
     }
     return render(request, "wiki/article_detail.html", context)
+
+@login_required
+def article_create(request):
+    form=ArticleForm
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = Article()
+            article.title = form.cleaned_data.get("title")
+            article.category.name = form.cleaned_data.get("category")
+            article = form.save()
+            return redirect("wiki:article_detail", pk=article.pk)
+        
+    ctx = {"articleform":form}
+    return render(request, 'wiki/article_create.html',ctx)
+
+@login_required
+def article_update(request, pk):
+    article = get_object_or_404(Article, id=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect("wiki:article_detail", pk=article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    
+    ctx = {"articleform":form}
+    return render(request, 'wiki/article_update.html',ctx)
