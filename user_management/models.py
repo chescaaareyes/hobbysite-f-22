bcd21fca0b60ea5c9bf98b2241777808c177bc6a
Francesca Reyes <francesca.dominique.reyes@student.ateneo.edu>
2024-05-05 19:19:47 +0800

    Add string representation for Profile model

--- user_management/models.py
+++ user_management/models.py
@@ -6,3 +6,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=63)
     email_address = models.EmailField(max_length=255)
+
+    def __str__(self):
+        return self.display_name
