29b000f624091651b0d43a6f504a91b2ec899abe
jamesmostajo <jipmostajo@gmail.com>
2024-05-06 00:04:21 +0800

    added temporary create view

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import product_detail, products_list
+from .views import product_detail, products_list, product_add
 
 urlpatterns = [
     path("items", products_list, name="product_list"),
     path("item/<int:pk>/", product_detail, name="product_detail"),
+    path("item/add/", product_add, name="product_add"),
 ]
 
 app_name = "merchstore"
--- merchstore/views.py
+++ merchstore/views.py
@@ -1,21 +1,75 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 
-from .models import Product, ProductType
+from .models import Product, ProductType, Transaction
+from .forms import TransactionForm, ProductForm
+from django.contrib.auth.decorators import login_required
 
-
+# @login_required
 def products_list(request):
-    products = Product.objects.all()
-    ctx = {"products_list": products}
+    your_shop = []
+    user_products = []
+    if request.user.is_authenticated:
+        username = request.user.profile.display_name
+        your_shop = Product.objects.filter(status = "on sale").filter(owner = request.user.profile)
+        for_sale = Product.objects.exclude(owner = request.user.profile).filter(status = "on sale")
+        user_products = Product.objects.exclude(status = "on sale").filter(owner = request.user.profile)
+        other_products = Product.objects.exclude(status = "on sale").exclude(owner = request.user.profile)
+    else:
+        username = "Anonymous"
+        for_sale = Product.objects.filter(status = "on sale")
+        other_products = Product.objects.exclude(status = "on sale")
+        
+    ctx = {
+        "username" : username,
+        "your_shop" : your_shop,
+        "user_products": user_products,
+        "others" : other_products,
+        "for_sale" : for_sale,
+    }
     return render(request, "merchstore/product_list.html", ctx)
 
-
+@login_required
 def product_detail(request, pk):
     product = Product.objects.get(pk=pk)
+    user = request.user.profile
+    username = request.user.profile.display_name
+
+    form = TransactionForm()
+    if request.method == "POST":
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transact = Transaction()
+            transact.buyer = user
+            transact.created_on = timezone.now()
+            transact.product = product
+            transact.amount = form.cleaned_data.get("amount")
+            transact.status = "on cart"
+            transact.save()
+            return redirect("user_management:home")
     ctx = {
+        "username" : username,
         "name": product.name,
+        "owner" : product.owner.display_name,
         "description": product.description,
         "price": product.price,
         "product_type": product.product_type.name,
+        "form" : form,
     }
     return render(request, "merchstore/product_detail.html", ctx)
+
+
+@login_required
+def product_add(request):
+    user = request.user.profile
+    form = ProductForm()
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid:
+            product = Product()
+            product.owner = user
+            product = form.save()
+            return redirect("merchstore:product_detail", pk=product.pk)
+    ctx = {"form" : form }
+    return render(request, "merchstore/product_add.html", ctx)
