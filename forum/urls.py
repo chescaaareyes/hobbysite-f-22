4d51e2e377fe88d1ec98ec34f221de63dec34d11
Arthur Lluisma <arthur.lluisma@student.ateneo.edu>
2024-05-05 21:41:23 +0800

    Add new forum URLs

--- forum/urls.py
+++ forum/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import forum_detail, forum_list
+from .views import forum_detail, forum_list, forum_create, forum_update
 
 urlpatterns = [
     path("threads/", forum_list, name="forum_list"),
     path("thread/<int:pk>", forum_detail, name="forum_detail"),
+    path("thread/<int:pk>/add", forum_create, name="forum_create"),
+    path("thread/<int:pk>/edit", forum_update, name="forum_update"),
 ]
 
 app_name = "forum"
