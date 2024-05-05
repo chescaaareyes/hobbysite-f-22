92d102af0a36d76d3fa43218944f6d5434e62b6a
jamesmostajo <jipmostajo@gmail.com>
2024-05-06 00:01:41 +0800

    added transaction model and other fields to product and product type

--- merchstore/models.py
+++ merchstore/models.py
@@ -18,8 +18,17 @@ class Product(models.Model):
     product_type = models.ForeignKey(
         "ProductType", on_delete=models.SET_NULL, null=True, related_name="product_type"
     )
+    owner = models.ForeignKey("user_management.Profile", on_delete=models.CASCADE, related_name="owner", null=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    STOCK_CHOICES = {
+        "available" : "Available",
+        "on sale" : "On Sale",
+        "out of stock" : "Out of Stock",
+    }
+    stock = models.IntegerField(default=0)
+    status = models.CharField(max_length=12, choices=STOCK_CHOICES, default=STOCK_CHOICES["available"])
 
     def __str__(self):
         return self.name
@@ -29,3 +38,23 @@ class Product(models.Model):
 
     class Meta:
         ordering = ["name"]
+
+
+class Transaction(models.Model):
+    buyer = models.ForeignKey("user_management.Profile", null=True, on_delete=models.SET_NULL, related_name="buyer")
+    product = models.ForeignKey("Product", null=True, on_delete=models.SET_NULL, related_name="product")
+    amount = models.IntegerField()
+    
+    STATUS_CHOICES = {
+        "on cart" : "On Cart",
+        "to pay" : "To Pay",
+        "to shop" : "To Shop",
+        "to receive" : "To Receive",
+        "delivered" : "Delivered",
+    }
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    created_on = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return f'{self.amount} {self.product.name} bought by {self.buyer.display_name}'
