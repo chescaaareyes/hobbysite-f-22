bbfd8db5af934c08a9ea2af7ad338ae9290662a2
jamesmostajo <jipmostajo@gmail.com>
2024-05-06 00:04:43 +0800

    added forms.py

---
+++ merchstore/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import Product, Transaction
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ["amount"]
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = "__all__"
