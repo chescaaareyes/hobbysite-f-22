f0a24e529b07822aca5e79d763ad64103a4a4d63
gigigar <133097398+gigigar@users.noreply.github.com>
2024-05-07 01:02:00 +0800

    updated templates

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
 
-# Article List View
+
 def articleList(request):
-    articles = Article.objects.order_by("-createdOn")
     if request.user.is_authenticated:
-        userArticles = articles.filter(author=request.user)
-        otherArticles = articles.exclude(author=request.user)
+        userArticles = Article.objects.filter(author=request.user).order_by("-createdOn")
+        otherArticles = Article.objects.exclude(author=request.user).order_by("-createdOn")
         context = {'userArticles': userArticles, 'otherArticles': otherArticles}
     else:
-        otherArticles = articles.all()
+        otherArticles = Article.objects.all().order_by("-createdOn")
         context = {'otherArticles': otherArticles}
     return render(request, 'blog/article_list.html', context)
 
-# Article Detail View
-def articleDetail(request, articleId):
-    article = get_object_or_404(Article, pk=articleId)
-    authorArticles = Article.objects.filter(author=article.author).exclude(pk=articleId)[:2]
+def articleDetail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    authorArticles = Article.objects.filter(author=article.author).exclude(pk=pk)[:2]
     comments = Comment.objects.filter(article=article).order_by('-createdOn')
     form = CommentForm()
     return render(request, 'blog/article_detail.html', {
@@ -28,25 +27,24 @@ def articleDetail(request, articleId):
         'form': form
     })
 
-# Article Create View
 @login_required
 def articleCreate(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST)
         if form.is_valid():
             article = form.save(commit=False)
-            article.author = request.user.profile
+            article.author = request.user
+            article.createdOn = timezone.now()
             article.save()
-            return redirect('article_detail', articleId=article.pk)
+            return redirect('blog:article_detail', pk=article.pk)
     else:
         form = ArticleForm()
     return render(request, 'blog/article_form.html', {'form': form})
 
-# Article Update View
 @login_required
 def articleUpdate(request, articleId):
     article = get_object_or_404(Article, pk=articleId)
-    if request.user.profile != article.author:
+    if request.user != article.author:
         return HttpResponse('You are not allowed to edit this article.')
     if request.method == 'POST':
         form = ArticleForm(request.POST, instance=article)
