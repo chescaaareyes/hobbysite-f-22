d902d0dc8a2e6b0261be29c4397e0a6b7014d5ca
gigigar <133097398+gigigar@users.noreply.github.com>
2024-05-06 23:43:43 +0800

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path("articles/", views.articleList, name="article_list"),
     path("article/<int:pk>/", views.articleDetail, name="article_detail"),
+    path("article/add/", views.articleCreate, name="article_create"), 
+    path("article/<int:pk>/edit/", views.articleUpdate, name="article_update"),  
 ]
 
 app_name = "blog"
